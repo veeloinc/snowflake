f83ac341027de61af09f52b4c8c810ba9be6453a
Chris Pitzer <cpitzer@mobilepaks.com>
2016-02-11 12:39:54 -0800

    a big, ugly react native fs test

--- src/components/react-native-fs-test/index.common.js
+++ src/components/react-native-fs-test/index.common.js
@@ -1,57 +1,10 @@
-"use strict";
-
-/*
- * ## Imports
- *  
- * Imports from redux
- */
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-
-// import {RNFSApp} from '../components/react-native-fs-test/index.common';
-
 /**
- * Immutable Map
+ * Sample React Native App
+ * https://github.com/facebook/react-native
  */
-import {Map} from 'immutable';
+'use strict';
 
-/**
- * Project imports
- */
-import ResourceCatalog from '../components/ResourceCatalog';
-import ResourceSearch from '../components/ResourceSearch';
-
-/**
- * Project actions
- */
-import * as deviceActions from '../reducers/device/deviceActions';
-import * as globalActions from '../reducers/global/globalActions';
-
-import React,
-{
-    Component,
-    StyleSheet,
-    View
-}
-    from 'react-native';
-
-const {
-    SEARCH_NOW,
-    SEARCH_VALUE_UPDATE
-} = require('../lib/constants').default;
-
-/**
- * ## Actions
- * 3 of our actions will be available as ```actions```
- */
-const actions = [
-    deviceActions,
-    globalActions
-];
-
-import ErrorAlert from '../components/ErrorAlert';
-
-
+var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
@@ -61,8 +14,9 @@ var {
   Image,
 } = React;
 
+import { RNFS} from 'react-native-fs-blablabla';
 
-var RNFS = require('../../node_modules/react-native-fs');
+// var RNFS = require('react-native-fs');
 
 var testDir1Path = RNFS.DocumentDirectoryPath + '/test-dir-1';
 var testFile1Path = RNFS.DocumentDirectoryPath + '/test-dir-1/test-file-1';
@@ -314,66 +268,4 @@ var styles = StyleSheet.create({
   },
 });
 
-
-/**
- *  Save all state as props (MC: ?!)
- */
-function mapStateToProps(state) {
-    return {
-        ...state
-    };
-}
-
-/*
- * Bind all the functions from the ```actions``` and bind them with
- * ```dispatch```
-
- */
-function mapDispatchToProps(dispatch) {
-
-    const creators = Map()
-        .merge(...actions)
-        .filter(value => typeof value === 'function')
-        .toObject();
-
-    return {
-        actions: bindActionCreators(creators, dispatch),
-        onSearchChange: (formValues) => {
-            dispatch({type: SEARCH_VALUE_UPDATE, payload: {field: "search", value: formValues.search}})
-        },
-        onSearch: () => {
-            dispatch({type: SEARCH_NOW});
-        },
-        dispatch
-    };
-}
-
-/**
- * ## App class
- */
-class App extends Component {
-    render() {
-        let content = [
-            {name: 'party', value: 'yes'},
-            {name: 'marty', value: 'sorta'},
-            {name: 'farty', value: 'what'}
-        ];
-        return (
-            <View>
-                <ResourceSearch
-                    value={this.props.resourceSearch.form.fields}
-                    onChange={this.props.onSearchChange}
-                    onButtonPress={this.props.onSearch}
-                    formOptions={this.props.resourceSearch.form}/>
-                <ResourceCatalog content={content}
-                                 filter={this.props.resourceSearch.currentSearchFilterOnResources}/>
-                <RNFSApp/>
-            </View>
-        );
-    }
-}
-
-/**
- * Connect the properties
- */
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// AppRegistry.registerComponent('ReactNativeFSTest', () => RNFSApp);
